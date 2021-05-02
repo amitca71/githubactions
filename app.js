f3991b4880db318e80669715ebd1674960340134
amit cahanovich <amitca71@gmail.com>
2021-05-02 22:36:58 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  const name = url.parse(req.url,true).pathname ;
+  console.log(name);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  response='Hello ';
+  if (name == '/'){
+      response=response.concat('world');
+  }else{
+      response=response.concat(name.substring(1));
+  }
+
+  res.end(response);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
