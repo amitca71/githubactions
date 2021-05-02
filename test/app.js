50b090a5788dc6f065f7481593c7a9fff64f8a1d
Amit Cahanovich <amit.cahanovich@jibe.solutions>
2021-05-02 22:21:44 +0100

    i

--- test/app.js
+++ test/app.js
@@ -2,7 +2,7 @@ var expect    = require("chai").expect;
 var request = require("request");
 var converter = require("../app.js");
 describe("check returns of function", function() {
-    var url = "http://localhost:3000/";
+    var url = "http://localhost:3001/";
     it("returns status 200", function() {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
