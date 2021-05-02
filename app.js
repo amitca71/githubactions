97bcf94dff7f6d63dc4fc85141a521f887eb6652
Amit Cahanovich <amit.cahanovich@jibe.solutions>
2021-05-02 23:02:14 +0100

    iadding docker and bind on 0.0.0.0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3001;
 
 const server = http.createServer((req, res) => {
