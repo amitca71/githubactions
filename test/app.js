cc84ff4667e448824f32b9fafb0c584ab89c46df
Amit Cahanovich <amit.cahanovich@jibe.solutions>
2021-05-03 08:07:29 +0100

    ii adding async to mocha

--- test/app.js
+++ test/app.js
@@ -1,11 +1,12 @@
 var expect    = require("chai").expect;
 var request = require("request");
 var converter = require("../app.js");
+var async =  require("async");
 describe("check returns of function", function() {
     var url = "http://localhost:3001/";
     it("returns status 200", function() {
       request(url, function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
+        expect(response.statusCode).to.equal(2001);
       });
     });
  }); 
